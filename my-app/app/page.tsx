bef980a0ea869662389b61395bdc1e68146c56a1
Piladda <107570496+YuyPPY@users.noreply.github.com>
2024-04-02 13:52:37 +0700

    yify
    iiooi

--- my-app/app/page.tsx
+++ my-app/app/page.tsx
@@ -45,6 +45,7 @@ export default function Home() {
           </h2>
           <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
           ljgdfnk;vbfnlknsd;kjfvb;osjdlnbvjlsdbk
+          fuivsg;ldhefcui;sljkdfclhbsdjkchkasd
           </p>
         </a>
 
